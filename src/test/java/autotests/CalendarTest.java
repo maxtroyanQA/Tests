d69735866a100b2c5cc59266fee90509b5f900aa
Max <m.troyan@qualitu-lab.ru>
2021-08-25 18:02:39 +0300

    9.1

--- src/test/java/autotests/CalendarTest.java
+++ src/test/java/autotests/CalendarTest.java
@@ -4,19 +4,27 @@ package autotests;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CalendarTest extends AuthorizedTestBase{
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class CalendarTest extends AuthorizedTestBase {
     @BeforeEach
-    void preconditionCalendarTest(){
+    void preconditionCalendarTest() {
         clickMenu()
                 .clickMenuCalendar()
-                .foundSiteCalendar(siteCalendar);
-    //sleep(10000);
+                .foundSiteCalendar(siteCalendar)
+                .loadCalendar();
     }
-@Test
-void calendarTest(){
-//получение месяца из даты системы (текущий месяц)
-       dataComparison();
 
-
-}
+    @Test
+    void calendarTest1() {
+        dataComparison()
+                .checkWorkDay()
+                .checkWeekendDay();
+    }
+    @Test
+    void calendarTest2(){
+        $x("//div[@class= 'input-group date filter_input_date']/span[@class='input-group-addon']").click();
+sleep(10000);
+    }
 }
--- src/test/java/autotests/LoginPage.java
+++ src/test/java/autotests/LoginPage.java
@@ -2,15 +2,18 @@ package autotests;
 
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.Calendar;
+import java.util.List;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.ownText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
 class  LoginPage extends TestBase {
@@ -34,8 +37,18 @@ class  LoginPage extends TestBase {
     protected SelenideElement buttonLogin = $("#_submit");
     protected SelenideElement buttonMenu = $("#m_aside_left_minimize_toggle");
     protected SelenideElement avatar = $("div[class='m-stack__item m-topbar__nav-wrapper']");
+    protected SelenideElement loadingCalendar = $("span[class='btn m-btn btn-primary m-loader m-loader--light m-loader--lg m-loader--left']");
+    protected SelenideElement table =$x("//tbody[@class='fc-body']");
+    protected ElementsCollection allDayTable = $$x("//div[@class='fc-day-grid fc-unselectable']" +
+            "// a[@class= 'fc-day-grid-event fc-h-event fc-event fc-start " +
+            "fc-end schedule-badge schedule-badge--block schedule-badge--default schedule-badge--']");
+    protected SelenideElement allDayCalendar = $x("//td[@class='fc-event-container']");
+//    protected ElementsCollection workDays = $$x("//div[@class='fc-day-grid fc-unselectable']" +
+//            "// a[@class= 'fc-day-grid-event fc-h-event fc-event fc-start " +
+//            "fc-end schedule-badge schedule-badge--block schedule-badge--default schedule-badge--']");
 
 
+    //ElementsCollection WORKDAY = $$x("//div[@class='fc-day-grid fc-unselectable']// a[@class= 'fc-day-grid-event fc-h-event fc-event fc-start fc-end schedule-badge schedule-badge--block schedule-badge--default schedule-badge--']");
     //метод ввода логина
     LoginPage setLogin(String setLoginSend) {
         login.sendKeys(setLoginSend);
@@ -89,18 +102,51 @@ class  LoginPage extends TestBase {
         return this;
     }
     LoginPage dataComparison (){
+        //получение из системы года и месяца на русском
         Calendar calendar = Calendar.getInstance();
         String[] monthNames = { "январь", "февраль", "март", "апрель",
-                "май", "июнь", "июль", "Август",
+                "май", "июнь", "июль", "август",
                 "сентябрь", "октябрь", "ноябрь", "декабрь"
         };
         String monthNow = monthNames[calendar.get(Calendar.MONTH)];
         String yearNow = String.valueOf(calendar.get(Calendar.YEAR));
-SelenideElement textData = $x("//span[@id='schedule-month-title']");
-System.out.println(textData);
-       textData.shouldHave(text(monthNow), text(yearNow));
+        //проверка, что есть дата соответствует
+        SelenideElement textData = $x("//span[@id='schedule-month-title']");
+        textData.shouldHave(ownText(monthNow), ownText(yearNow));
         return this;
-
-
+    }
+    LoginPage loadCalendar (){
+        loadingCalendar.waitUntil(Condition.not(visible),60000);
+        return this;
+    }
+    LoginPage checkWorkDay (){
+        List<WebElement> workDays = allDayCalendar.findElements
+                (By.xpath("//a[@class='fc-day-grid-event fc-h-event fc-event fc-start fc-end schedule-badge " +
+                        "schedule-badge--block schedule-badge--default schedule-badge--']"));
+        for (WebElement workDay : workDays) {
+            if (workDays.size()>0){
+            }
+            else{
+                System.out.println("no working days");
+            }
+        }
+        return this;
+    }
+    LoginPage checkWeekendDay (){
+        List<WebElement> weekendDays = allDayCalendar.findElements
+                (By.xpath("//a[@class='fc-day-grid-event fc-h-event fc-event fc-start fc-end schedule-badge " +
+                        "schedule-badge--block schedule-badge--no-event schedule-badge--']"));
+        for (WebElement weekendDay : weekendDays) {
+            if (weekendDays.size()>0){
+            }
+            else{
+                System.out.println("no weekend days");
+            }
+        }
+        return this;
+    }
+    LoginPage choiceNextMonth (){
+System.out.println("ok");
+return this;
     }
 }

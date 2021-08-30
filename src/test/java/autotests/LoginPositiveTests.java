ebd8d39aa69e1cd1f17c89d8cc5ffc6a917a5cf1
Max <m.troyan@qualitu-lab.ru>
2021-08-30 10:10:45 +0300

    11.1

--- src/test/java/autotests/LoginPositiveTests.java
+++ src/test/java/autotests/LoginPositiveTests.java
@@ -4,16 +4,23 @@ package autotests;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class LoginPositiveTests extends TestBase {
     TypesProperties properties = new TypesProperties();
     @Test
     void PositiveTests() throws Exception {
-        authorized.Authorized();
+        open(properties.startSITE);     //Переход на сайт указанный
+
+        //method chaining (цепочки вызовов)
+        loginPage.setLOGIN(loginPage.USERLOGIN)
+                .setPass(loginPage.USERPASSWORD)
+                .clickButton();
 
         loginPage.foundSiteEdit(loginPage.SITEEDIT)
                  .clickUserAvatar();
         //сравнение введенного имени пользователя и пароля
-        loginPage.NAME.shouldHave(Condition.text(properties.LOGIN_P));
+        loginPage.NAME.shouldHave(Condition.text(loginPage.USERLOGIN));
         loginPage.EMAIL.shouldHave(Condition.text(loginPage.USEREMAIL));
     }
 

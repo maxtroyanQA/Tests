2a8d1738d6c2aec6e0a1c71e38e2cc0ac6131c78
Max <m.troyan@qualitu-lab.ru>
2021-08-31 18:10:02 +0300

    12.2

--- src/test/java/autotests/LoginPositiveTests.java
+++ src/test/java/autotests/LoginPositiveTests.java
@@ -4,66 +4,27 @@ package autotests;
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Selenide.open;
 
-
- class LoginPositiveTests extends TestBase {
-
-
-String a="Test";
-String b="Test1";
-
-     //
-//    private static Object[] data(){
-//        return new Object[][]{
-//                {properties.LOGIN_TEST1,properties.PASSWORD_TEST1},
-//                {properties.LOGIN_TEST2,properties.PASSWORD_TEST2},
-//                {properties.LOGIN_TEST3,properties.PASSWORD_TEST3}
-//        };
-//    }
-//     @Test
-//     @Parameterized.Parameters
-//     public static Collection4 primeNumbers() {
-//         return Arrays.asList(new Object[][] {
-//                 { "#dff0d8", "//div[@class='alert alert-success alert-normal-success']", "×\nI'm a normal success message. To close use the appropriate button.","Normal success message", "1" },
-//                 { "#fcf8e3", "//div[@class='alert alert-warning alert-normal-warning']", "×\nI'm a normal warning message. To close use the appropriate button.", "Normal warning message", "2" },
-//                 { "#f2dede", "//div[@class='alert alert-danger alert-normal-danger']", "×\nI'm a normal danger message. To close use the appropriate button.", "Normal danger message", "3" },
-//                 { "#d9edf7", "//div[@class='alert alert-info alert-normal-info']", "×\nI'm a normal info message. To close use the appropriate button.", "Normal info message", "4" }
-//         });
-//     }
-
-
-
-
-
-     //@ExtendWith
-
-
-     public Collection data (){
-         return new ArrayList(Collections.singleton(new Object[][]{
-                 {properties.LOGIN_TEST1, properties.PASSWORD_TEST1},
-                 {properties.LOGIN_TEST2, properties.PASSWORD_TEST3}
-         }));
-     }
+@DisplayName("Позитивный тест")
+@ExtendWith(ScreenshotExtension.class)
+ public class LoginPositiveTests extends TestBase {
 
 @ParameterizedTest
-@DisplayName("Ввод правильного логина и пароля с проверкой авторизации")
-//@MethodSource("data")
-@Attachment
-//@CsvSource(value = {
-//        "Тест, Т",
-//        "Авто Пользователь, 1"
-//})
-@MethodSource("data")
 
-     //@ArgumentsSource(MyArguments.class)
+@DisplayName("Ввод правильного логина и пароля с проверкой авторизации")
+
+@Attachment
+
+@CsvSource(value = {
+        "Тест, Т",
+        "Авто Пользователь, 12345678"
+})
+
      public void PositiveTests(String login, String pass) {
          open(properties.startSITE);     //Переход на сайт указанный
 
